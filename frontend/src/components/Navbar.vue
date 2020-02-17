7d2b84f4c9ef28dc22457e384c2279ffbee1ecbb
zthulj <jeremy.thulliez@gmail.com>
2020-02-17 19:42:34 +0100

    feat(front): Navbar test

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,89 @@
+<template>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <router-link to="/" tabindex="0" class="navbar-item logo-title">
+        <b>Wootlab</b>.io
+      </router-link>
+      <a tabindex="0" role="button" class='navbar-burger burger' id="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start">
+      </div>
+      <div class="navbar-end">
+        <router-link to="blog" tabindex="0" class="navbar-item link-nav">
+          Blog
+        </router-link>
+        <router-link to="blog" tabindex="0" class="navbar-item link-nav">
+          Projects
+        </router-link>
+        <router-link to="admin" tabindex="0" class="navbar-item link-nav">
+          Admin
+        </router-link>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  mounted () {
+    const navBarBurger = document.getElementById('navbar-burger burger')
+    navBarBurger.addEventListener('click', () => {
+      const target = navBarBurger.dataset.target
+      const $target = document.getElementById(target)
+      navBarBurger.classList.toggle('is-active')
+      $target.classList.toggle('is-active')
+    })
+  }
+}
+</script>
+
+<style scoped type="scss">
+  .navbar-burger{
+    height:4.25em;
+  }
+  .logo-title{
+    font-size:28px;
+  }
+
+  .link-nav {
+    color: rgba(40, 38, 49, 0.9);
+    height: 3.5rem;
+    line-height: 3.5rem;
+    padding: 0px 1rem;
+    position: relative;
+    display: flex;
+    -moz-box-pack: center;
+    justify-content: center;
+  }
+
+  .link-nav::before {
+    position: absolute;
+    content: " ";
+    display: block;
+    width: 0px;
+    background-color: #4a6fa5;
+    height: 4px;
+    bottom: 0.6rem;
+    transition: width 0.2s ease-in-out 0s;
+  }
+
+  .link-nav:hover::before {
+    width: 50%;
+  }
+
+  @media (max-width: 1024px) {
+    .link-nav{
+      font-size: 2rem;
+    }
+    .link-nav:hover::before {
+      width: 0%;
+    }
+
+  }
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,117 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="home-container">
+      <div class="">
+        <h1 class="title">
+          <span class="main">Wootlab</span>
+          <span class="subtitle">Blog - Projects - Tools </span>
+        </h1>
+        <span class="description">A random passionate developper's place</span>
+        <div class="btns-list-container">
+            <router-link to="/news" class="home-btn">Blog</router-link>
+            <router-link to="/doc" class="home-btn">Projects</router-link>
+            <a href="https://twitter.com/zthulj" target="_blank" class="home-btn">Twitter</a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+  name: 'home'
 }
 </script>
+<style type="scss" scoped>
+  .home-container {
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1.5;
+    display: flex;
+    margin: 4rem 0px 2rem;
+  }
+
+  .home {
+    position: relative;
+    width: 100%;
+    display: flex;
+    -moz-box-pack: center;
+    justify-content: center;
+    -moz-box-align: center;
+    align-items: center;
+    overflow: hidden;
+    background-color: #166088;
+    min-height: 100vh;
+    font-family: "Arvo", sans-serif;
+  }
+
+  .home-btns {
+    margin-top:15px;
+    display: flex;
+    font-size: 1.1em;
+    font-weight: bold;
+    z-index: 1;
+    list-style: none;
+  }
+
+  .home-btn{
+    border: 2px solid
+    rgb(255, 255, 255);
+    border-radius: 8px;
+    padding: 0.5em 1em;
+    color:
+      rgb(255, 255, 255);
+    transition-duration: 0.15s;
+    transition-timing-function: ease-in-out;
+    transition-delay: 0s;
+    transition-property: background-color, color;
+    margin-left: 1.6em;
+  }
+
+  .home-btn a{
+    color:white;
+  }
+
+  .home-btn:hover{
+    background-color:
+      rgb(255, 255, 255);
+    color:black !important;
+  }
+
+  .home-btn:hover a{
+    color:black;
+  }
+
+  .btns-list-container{
+    margin-top:25px;
+  }
+
+  .main {
+    font-size: 4.2em;
+    font-weight: bold;
+  }
+
+  .title {
+    font-size: 1.2em;
+    position: relative;
+    padding-bottom: 1.2em;
+    margin-bottom: 0.8rem;
+    color: white;
+  }
+
+  .subtitle {
+    font-size: 1.2em;
+    position: absolute;
+    left: 20px;
+    bottom: 0px;
+    color: white;
+    font-weight:400;
+  }
+
+  .description {
+    text-align: center;
+    opacity: 0.7;
+    max-width: 80vw;
+    color: white;
+  }
+</style>
