4dd39bd38704f34a5545a80e384c035f2f59e09a
grzi <jeremy.thulliez@gmail.com>
2020-03-09 22:43:13 +0100

    feat(build): Prod backend project id

--- data-exposition/src/main/java/io/wootlab/data/Application.java
+++ data-exposition/src/main/java/io/wootlab/data/Application.java
@@ -2,6 +2,7 @@ package io.wootlab.data;
 
 import io.micronaut.runtime.Micronaut;
 
+
 public class Application {
     public static void main(String[] args) {
         Micronaut.run(Application.class);
